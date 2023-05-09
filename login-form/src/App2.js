7925ac0cc7a3412f857a420a35568798b648b08a
KelsieAvella <kavella@carthage.edu>
2023-05-09 10:06:31 -0500

    added login

--- login-form/src/App2.js
+++ login-form/src/App2.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import About from './About';
+import Form from './Form'; //added form for testing
 import Home from './Home';
 import Login from './Login';
 import ShowNavbar from './ShowNavbar';
+
 import { Route, Routes } from 'react-router-dom';
 
 
@@ -20,6 +22,7 @@ class App extends Component {
            <Route path="/" element={<Home />} />
            <Route path="/about" element={<About />} />
            <Route path="/login" element={<Login />} />
+           <Route path="/form" element={<Form />} />
          </Routes>
      </>
     )
