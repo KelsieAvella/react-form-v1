e1ac0f4a6504b86476788135c033079c4d0c47a0
KelsieAvella <kavella@carthage.edu>
2023-05-21 18:53:41 -0500

    sdk

--- signup-form/src/About.js
+++ signup-form/src/About.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import notes from './images/notes.png'
+import board from './images/board-stickynotes.jpg'
 import './About.css'
 
 const About = () => {
     return (
         <div className='about' id='about'>
             <div className='container'>
-                <img src={notes} alt='john' />
+                <img src={board} />
                 <div className='col-2'>
                     <h1>About</h1>
                     <span className='line'></span>
-                    <p>Whether you're a college grad looking for a career or a highschool grad looking for colleges, Lifetrack is the right fit for you! </p>
+                    <p>Whether you're a college grad on the job hunt or a highschool grad looking to apply for colleges, Lifetrack is the right fit for you! It doesn't matter where you are in your journey, Lifetrack can help get you there! </p>
     
                 </div>
             </div>
