f3c1923f91a4c90768f0ea3043233d819b094894
KelsieAvella <kavella@carthage.edu>
2023-05-22 13:19:59 -0500

    shdjks

--- signup-form/src/About.js
+++ signup-form/src/About.js
@@ -1,21 +1,30 @@
 import React from "react";
 import board from './images/board-stickynotes.jpg'
-import "./About.css";
+import note from './images/pushpinNote.png'
+import paperclip from './images/paperclipNote.png'
+import Card from './Card' 
+
+
+
+
 function About() {
-  return (
-    <div className="about">
-      <div
-        className="aboutTop"
-        style={{ backgroundImage: `url(${board})` }}
-      ></div>
-      <div className="aboutBottom">
-        <h1> About Lifetrack</h1>
-        <p>
-        Whether you're a college grad on the job hunt or a highschool grad looking to apply for colleges, Lifetrack is the right fit for you! It doesn't matter where you are in your journey, Lifetrack can help get you there!
-        </p>
+    return (
+      <div className="about">
+        <div className="aboutTop" style={{ backgroundImage: `url(${board})` }}></div>
+        <div className="aboutBottom">
+          <h1> About Lifetrack</h1>
+          <div className="imageContainer">
+                  Whether you're a college grad on the job hunt or a high school grad looking to apply for colleges, Lifetrack is the right fit for you! It doesn't matter where you are in your journey, Lifetrack can help get you there!
+          </div>
+          <h1> How It Works </h1>
+          <p>
+            Once you've made an account with us and logged in, you have access to a personal dashboard where you can start creating goals and tracking your progress. Each 
+          </p>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
 
 export default About;
+
